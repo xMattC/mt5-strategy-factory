5ead113db5e6ff578995f43ef584d26178c21e44
Matt Corcoran <mkcorcoran2015@gmail.com>
2025-05-07 13:39:08 +0200

    Update to pathing methods

--- meta_strategist/main.py
+++ meta_strategist/main.py
@@ -2,25 +2,21 @@ from meta_strategist.pipeline.stages import get_stage
 from meta_strategist.generators.ini_generator import IniConfig
 from meta_strategist.pipeline.optimisation import Optimization
 
-CONFIG = IniConfig(
-    run_name='Apollo',
-    start_date="2023.01.01",
-    end_date="2023.12.31",
-    period="D1",
-    custom_criteria="ProfitFactor",
-    symbol_mode="ALL",
-    data_split="year",
-    risk=0.01,
-    sl=50,
-    tp=100
-)
-STAGE = get_stage("C1")
-
-
-def main():
-    pipeline = Optimization(CONFIG, STAGE)
-    pipeline.run_optimisation()
-
-
 if __name__ == "__main__":
-    main()
+    CONFIG = IniConfig(
+        run_name='Apollo',
+        start_date="2023.01.01",
+        end_date="2023.12.31",
+        period="D1",
+        custom_criteria="ProfitFactor",
+        symbol_mode="ALL",
+        data_split="year",
+        risk=0.02,
+        sl=2,
+        tp=1
+    )
+
+    STAGE = get_stage("Trigger")
+
+    pipeline = Optimization(config=CONFIG, stage=STAGE)
+    pipeline.run_optimisation()
